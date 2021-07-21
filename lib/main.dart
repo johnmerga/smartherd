4bce7515e75bb515df12916fe21c64ecf777709a
johnmerga <johnmerga@yahoo.com>
2021-07-22 02:01:47 +0300

    appBar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Center(
-    child: Text(
-      "Your First Text  ",
-      textDirection: TextDirection.ltr,
+  runApp(MaterialApp(
+    title: "First App",
+    home: 
+    Scaffold(appBar: AppBar(title: Text("appBar", style:TextStyle(fontSize: 27.0),),),
+    body: Material(
+      color: Colors.blue[800],
+      child: Center(
+          child: Text(
+            "Your First Text  ",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white,fontSize: 40.0 ),
+        ),
+      ),
     ),
+    )
+    
+    
   ));
-}
+}  
